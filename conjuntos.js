c1349a3ffcd0338ecb796c035eec6cee78e9a894
leticr86 <129956311+leticr86@users.noreply.github.com>
2023-04-21 11:20:37 +0200

    Create  conjuntos.js

---
+++ conjuntos.js
@@ -0,0 +1,8 @@
+// Crear un Set con los nombres de tu familia
+const familia = new Set(['Juan', 'María', 'Pedro', 'Ana']);
+
+// Añadir un nombre duplicado al Set
+familia.add('Juan');
+
+// Añadir el nombre "Javascript" al Set
+familia.add('Javascript');
